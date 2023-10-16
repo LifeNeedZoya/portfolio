97d8c09ac9cecee99f4fb2fc76f61acf4809e2ce
Zoya <114851044+tsrenyanjin@users.noreply.github.com>
2023-10-16 15:01:49 +0800

    first commit

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,49 +1,24 @@
 "use client";
-import React from "react";
-import { Model } from "./House";
-import { Typewriter } from "react-simple-typewriter";
-import { Canvas } from "@react-three/fiber";
-import { Stage } from "@react-three/drei";
-import { OrbitControls } from "@react-three/drei";
+import React, { useEffect, useRef } from "react";
+
 const HeroSection = () => {
   return (
-    <main>
-      <section className="h-screen snap-always snap-center bg-[url('/images/bg.jpeg')] ">
-        <div className="  container mx-auto align ">
-          <Canvas>
-            <Stage environment="city" intensity={0.8}>
-              <Model />
-            </Stage>
-            <OrbitControls enableZoom={true} />
-          </Canvas>
-          <span>
-            <Typewriter
-              words={[
-                "UX Designer",
-                "UI Designer",
-                "Front-end developer",
-                "Back-end developer",
-                "Based in UB",
-              ]}
-              loop={5}
-              cursor
-              cursorStyle="..."
-              typeSpeed={70}
-              deleteSpeed={50}
-              delaySpeed={2000}
-            />
-          </span>
-
-          <div>
-            <button className="px-6 py-4 rounded-full mr-4 w-full sm:w-fit bg-white hover:bg-gradient-to-br text-black from-green-800 via-slate-800 to-black">
-              Hire me
-            </button>
-            <button className="px-6 py-4 rounded-full bg-transparent sm:w-fit w-full  text-white border-white border mt-3 hover:bg-slate-800">
-              Download CV
-            </button>
-          </div>
+    <main className=" h-screen w-screen bg-[var(--bgColor)] flex">
+      <div
+        className=" inline-block m
+        flex-1   "
+      >
+        <div className="flex justify-center items-center flex-col text-[var(--textColor)] text-9xl font-[100px]">
+          <h1 className="text-[var(--textColor)] helloClass text">Hello</h1>
+          <h2 className="">Zoya</h2>
+          <h2 className="">Back-end</h2>
+          <h2 className="">Front-end</h2>
+          <h2 className="">developer</h2>
         </div>
-      </section>
+      </div>
+      <div className="flex-1">
+        <img src="/images/hero-image.png" alt="" />
+      </div>
     </main>
   );
 };
